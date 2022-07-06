1f18bbc3e99a2010a1ca0c63dadc033c98b974b2
asdf4026 <i1427@naver.com>
2022-07-06 21:53:29 +0900

    오늘공부

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -32,14 +32,22 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public boolean updataUser() throws Exception {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean deleteUser() throws Exception {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public User loadUser(String username, String password) throws Exception {
+		User user = userDao.findUserByUsername(username);
+		if(user == null) {
+			return null;
+		}else {
+			return user.getPassword().equals(password) ? user : null;
+		}
 	}
 
 
--- src/main/webapp/static/js/signup.js
+++ src/main/webapp/static/js/signup.js
@@ -23,6 +23,9 @@ submitButton.onclick = () =>{
 		alert("사용자 이름 중복확인이 필요합니다.");
 		return;
 	}
+	if(!signupFlag.includes(false)){
+      submit();
+   }
 }
 
 usernameInput.onblur = () => {
@@ -58,11 +61,19 @@ function submit(){
 		},
 		dataType: "text",
 		success: (response) =>{
-			
+			if(response == "true"){
+				alert("축하합니다!\n 회원가입에 성공하였습니다.");
+				location.replace("/signin");
+			}else{
+				alert("회원가입에 실패하였습니다.\n다시 시도해 주세요." );
+				usernameCheckMsg.innerHTML = ``;
+				document.querySelector("form").reset();
+			}
 		},
 		error:errorMessage
 	});
 }
+
 function errorMessage(request, status, error){
 		alert("요청 실패");
 		console.log(request.status);
